b3ad4321a7471371769d9d1af0988183fd37f40c
Julian Rubisch <julian@julianrubisch.at>
2022-01-09 14:35:21 +0100

    Install cubism

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import { cable } from "@hotwired/turbo-rails";
+
+import CableReady from "cable_ready";
+import "@minthesize/cubism";
+
+(async () => {
+  const consumer = await cable.getConsumer()
+  CableReady.initialize({ consumer });
+})()
