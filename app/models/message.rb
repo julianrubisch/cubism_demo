717b6ebc4a0c203d0eb9272b00d1793fdfc74cb1
Julian Rubisch <julian@julianrubisch.at>
2022-01-09 14:40:31 +0100

    Include Cubism modules

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
+  # tracks users present on an instance of this model
+  include Cubism::Presence
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # establishes the class Cubism uses for user lookup
+  include Cubism::User
 end
